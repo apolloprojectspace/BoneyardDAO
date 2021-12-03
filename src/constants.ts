3f4130be655dc1d2b1debaf0a279613d2f9cb948
blockchainGuru125 <workerkit001@gmail.com>
2021-12-04 02:03:00 +1000

    update message

--- src/constants.ts
+++ src/constants.ts
@@ -30,9 +30,9 @@ export const addresses: IAddresses = {
   },
 };
 export const messages = {
-  please_connect: "Please connect your wallet to the Avalanche network to use Wonderland.",
+  please_connect: "Please connect your wallet to the Fantom network to use Wonderland.",
   please_connect_wallet: "Please connect your wallet.",
-  try_mint_more: (value: string) => `You're trying to mint more than the maximum payout available! The maximum mint payout is ${value} CROP.`,
+  try_mint_more: (value: string) => `You're trying to mint more than the maximum payout available! The maximum mint payout is ${value} HEC.`,
   before_minting: "Before minting, enter a value.",
   existing_mint:
       "You have an existing mint. Minting will reset your vesting period and forfeit any pending claimable rewards. We recommend claiming rewards first or using a fresh wallet. Do you still wish to proceed?",
@@ -42,7 +42,7 @@ export const messages = {
   your_balance_updated: "Your balance was successfully updated",
   nothing_to_claim: "You have nothing to claim",
   something_wrong: "Something went wrong",
-  switch_to_avalanche: "Switch to the Avalanche network?",
+  switch_to_fantom: "Switch to the Fantom network?",
   slippage_too_small: "Slippage too small",
   slippage_too_big: "Slippage too big",
   your_balance_update_soon: "Your balance will update soon",
