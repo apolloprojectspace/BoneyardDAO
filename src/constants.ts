8eaae6a91933cdac1e9b670abcc661e52d6012c8
cwhittaker123 <chriswhittaker123@gmail.com>
2022-01-11 21:30:06 -0500

    Updating subgraph endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/wkich/hector-subgraph";
+export const THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/id/QmdW8CvS1zdKzSvb2xPWsUTWTpgVThRz6SC66wRneZLGWx";
 export const EPOCH_INTERVAL = 28800;
 
 // NOTE could get this from an outside source since it changes slightly over time
